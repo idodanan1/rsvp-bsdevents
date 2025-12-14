01623e14731ddade4120578bd3c49384484af6dc
idodanan1 <idodanan1@gmail.com>
2025-12-15 00:28:41 +0200

    Update EventManagement.tsx

--- src/components/EventManagement.tsx
+++ src/components/EventManagement.tsx
@@ -2993,9 +2993,9 @@ const EventManagement: React.FC = () => {
           </div>
         </div>
       )}
-     </div>
-   </div>
- );
+      </div>
+    </div>
+  );
   } catch (error) {
     console.error('Error rendering EventManagement:', error);
     return (
