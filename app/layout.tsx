d2a77460cd31d447f9cfb953be516a396ca3dcc2
idodanan1 <idodanan1@gmail.com>
2025-12-23 23:50:17 +0200

    0.0.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin', 'hebrew'] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'RSVP SaaS Platform',
