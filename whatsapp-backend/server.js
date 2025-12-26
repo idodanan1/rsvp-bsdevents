8f4b091cb4fc1a1b53e48a5a241ae6c65411d113
idodanan1 <idodanan1@gmail.com>
2025-12-27 00:55:11 +0200

    אאי

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const multer = require('multer');
+const fs = require('fs');
 const path = require('path');
 const stripe = require('stripe');
 const { createClient } = require('@supabase/supabase-js');
